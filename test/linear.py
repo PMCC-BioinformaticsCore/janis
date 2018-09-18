3760c61ab8c1ed5d13cf34f519c9ec4e567542e2
Evan Thomas <thomas.e@wehi.edu.au>
2018-09-18 15:54:28 +1000

    - fix linear test

--- test/linear.py
+++ test/linear.py
@@ -30,7 +30,160 @@ steps:
       call:
 """
 
-_expected = json.loads("""""")
+_expected = json.loads("""{
+    "workflow": {
+        "inputs": {
+            "fastq": {
+                "type": "SequenceReadArchivePaired"
+            },
+            "ref": {
+                "type": "bam"
+            }
+        },
+        "flow": {
+            "untagged": {
+                "steps": {
+                    "1": {
+                        "step": "step1",
+                        "type": "trim",
+                        "step-inputs": {
+                            "read": {
+                                "type": "SequenceReadArchivePaired",
+                                "mapping": {
+                                    "provided": "",
+                                    "candidates": {
+                                        "1": {
+                                            "id": "fastq",
+                                            "type": "SequenceReadArchivePaired",
+                                            "step": "input-step",
+                                            "tag": "input"
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        "step-outputs": {
+                            "trimmed": {
+                                "type": "SequenceReadArchivePaired"
+                            }
+                        }
+                    },
+                    "2": {
+                        "step": "step2",
+                        "type": "align",
+                        "step-inputs": {
+                            "read": {
+                                "type": "SequenceReadArchivePaired",
+                                "mapping": {
+                                    "provided": "",
+                                    "candidates": {
+                                        "1": {
+                                            "id": "trimmed",
+                                            "type": "SequenceReadArchivePaired",
+                                            "step": "step1",
+                                            "tag": "untagged"
+                                        },
+                                        "2": {
+                                            "id": "fastq",
+                                            "type": "SequenceReadArchivePaired",
+                                            "step": "input-step",
+                                            "tag": "input"
+                                        }
+                                    }
+                                }
+                            },
+                            "reference": {
+                                "type": "reference",
+                                "mapping": {
+                                    "provided": "",
+                                    "candidates": {
+                                        "ERROR": "Failed to find an input candidate."
+                                    }
+                                }
+                            }
+                        },
+                        "step-outputs": {
+                            "alignedbamfile": {
+                                "type": "bam"
+                            }
+                        }
+                    },
+                    "3": {
+                        "step": "step3",
+                        "type": "dedup",
+                        "step-inputs": {
+                            "bamfile": {
+                                "type": "bam",
+                                "mapping": {
+                                    "provided": "",
+                                    "candidates": {
+                                        "1": {
+                                            "id": "alignedbamfile",
+                                            "type": "bam",
+                                            "step": "step2",
+                                            "tag": "untagged"
+                                        },
+                                        "2": {
+                                            "id": "ref",
+                                            "type": "bam",
+                                            "step": "input-step",
+                                            "tag": "input"
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        "step-outputs": {
+                            "bamfile": {
+                                "type": "bam"
+                            }
+                        }
+                    },
+                    "4": {
+                        "step": "step4",
+                        "type": "call",
+                        "step-inputs": {
+                            "alignedbamfile": {
+                                "type": "bam",
+                                "mapping": {
+                                    "provided": "",
+                                    "candidates": {
+                                        "1": {
+                                            "id": "bamfile",
+                                            "type": "bam",
+                                            "step": "step3",
+                                            "tag": "untagged"
+                                        },
+                                        "2": {
+                                            "id": "alignedbamfile",
+                                            "type": "bam",
+                                            "step": "step2",
+                                            "tag": "untagged"
+                                        }
+                                    }
+                                }
+                            },
+                            "reference": {
+                                "type": "reference",
+                                "mapping": {
+                                    "provided": "",
+                                    "candidates": {
+                                        "ERROR": "Failed to find an input candidate."
+                                    }
+                                }
+                            }
+                        },
+                        "step-outputs": {
+                            "read": {
+                                "type": "VCF"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}""")
 
 
 class LinearPipeline(unittest.TestCase):
@@ -40,10 +193,7 @@ class LinearPipeline(unittest.TestCase):
     translator.translate_string(_yml)
     translation = translator.pipeline()
     tr_json = json.loads(translation)
-    print('-'*80)
-    print(translation)
-    print('-'*80)
-    self.assertTrue(translation == _expected)
+    self.assertTrue(tr_json == _expected)
 
 
 if __name__ == '__main__':

a524307833a06d0bd0ecad3c8163c9e59435764f
franklinmichael <michael.franklin@petermac.org>
2018-12-14 10:39:30 +1100

    Adds PicardSortSam tool

---
+++ Pipeline/bioinformatics/data_types/bai.py
@@ -0,0 +1,12 @@
+from Pipeline import File
+
+
+class Bai(File):
+
+    @staticmethod
+    def name():
+        return "BAI"
+
+    @staticmethod
+    def doc():
+        return "Indexed BAM file (https://www.biostars.org/p/15847/), http://software.broadinstitute.org/software/igv/bam"
--- Pipeline/tests/workflows/test_parallel.py
+++ Pipeline/tests/workflows/test_parallel.py
@@ -4,6 +4,7 @@ from Pipeline import Workflow, Input, String, Step
 from Pipeline.bioinformatics.data_types.fasta import Fasta
 from Pipeline.bioinformatics.data_types.fastq import Fastq
 from Pipeline.bioinformatics.steps.bwa_mem import BwaMem
+from Pipeline.bioinformatics.steps.picard_sortsam import PicardSortSam
 from Pipeline.bioinformatics.steps.samtools import SamTools
 
 
@@ -17,10 +18,20 @@ class TestParallel(unittest.TestCase):
         sub_inp_bwa_reads = Input("reads", Fastq())
         sub_inp_bwa_read_group = Input("bwa_readGroup", String())
 
+        sub_inp_sortsam_valstrin = Input("sortsam_validation_stringency", String())
+
         step1_bwa_mem = Step("bwa_mem", BwaMem())
-
         step2_samtools= Step("samtools", SamTools())
+        step3_sortsam = Step("sortsam", PicardSortSam())
 
+        subworkflow.add_edge(sub_inp_bwa_ref, step1_bwa_mem)
+        subworkflow.add_edge(sub_inp_bwa_reads, step1_bwa_mem)
+
+        subworkflow.add_edge(sub_inp_bwa_read_group, step2_samtools)
+        subworkflow.add_edge(step1_bwa_mem, step2_samtools)
+
+        subworkflow.add_edge(sub_inp_sortsam_valstrin, step3_sortsam)
+        subworkflow.add_edge(step2_samtools, step3_sortsam)
 
 
     def test_parallel(self):

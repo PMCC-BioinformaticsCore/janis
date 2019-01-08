90f5c3d6a7fcd34638b7d3d8f683c74f26c49db1
Michael Franklin <michael@illusional.net>
2019-01-09 10:59:08 +1100

    Un-comments genome-germline pipeline

--- Pipeline/tests/workflows/wholegenomegermline.py
+++ Pipeline/tests/workflows/wholegenomegermline.py
@@ -1,6 +1,6 @@
 import unittest
 
-from Pipeline import Input, String, Step, Directory, Workflow, Array, Output, Int, File
+from Pipeline import Input, String, Step, Directory, Workflow, Array, Output
 from Pipeline.bioinformatics.data_types.bam import Bam
 from Pipeline.bioinformatics.data_types.bampair import BamPair
 from Pipeline.bioinformatics.data_types.fasta import Fasta
@@ -11,15 +11,17 @@ from Pipeline.bioinformatics.data_types.vcf import VcfIdx, TabixIdx
 from Pipeline.bioinformatics.tools.bcftools.norm.latest import BcfToolsNormLatest as BcfToolsNorm
 from Pipeline.bioinformatics.tools.bwa.mem.latest import BwaMemLatest as BwaMem
 from Pipeline.bioinformatics.tools.gatk4.applybqsr.latest import Gatk4ApplyBqsrLatest as Gatk4ApplyBqsr
-from Pipeline.bioinformatics.tools.gatk4.baserecalibrator.latest import Gatk4BaseRecalibratorLatest as Gatk4BaseRecalibrator
-from Pipeline.bioinformatics.tools.gatk4.genotypeconcordance.latest import Gatk4GenotypeConcordanceLatest as Gatk4GenotypeConcordance
-from Pipeline.bioinformatics.tools.gatk4.haplotypecaller.latest import Gatk4HaplotypeCallerLatest as Gatk4HaplotypeCaller
+from Pipeline.bioinformatics.tools.gatk4.baserecalibrator.latest import \
+    Gatk4BaseRecalibratorLatest as Gatk4BaseRecalibrator
+from Pipeline.bioinformatics.tools.gatk4.genotypeconcordance.latest import \
+    Gatk4GenotypeConcordanceLatest as Gatk4GenotypeConcordance
+from Pipeline.bioinformatics.tools.gatk4.haplotypecaller.latest import \
+    Gatk4HaplotypeCallerLatest as Gatk4HaplotypeCaller
 from Pipeline.bioinformatics.tools.gatk4.markduplicates.latest import Gatk4MarkDuplicatesLatest as Gatk4MarkDuplicates
 from Pipeline.bioinformatics.tools.gatk4.mergesamfiles.latest import Gatk4MergeSamFilesLatest as Gatk4MergeSamFiles
 from Pipeline.bioinformatics.tools.gatk4.sortsam.latest import Gatk4SortSamLatest as Gatk4SortSam
-from Pipeline.bioinformatics.tools.htslib.bgzip.latest import BGZipLatest
-from Pipeline.bioinformatics.tools.htslib.tabix.latest import TabixLatest
-from Pipeline.bioinformatics.tools.igvtools.index.latest import IgvToolsIndexLatest as IgvToolsIndex
+from Pipeline.bioinformatics.tools.htslib.bgzip.latest import BGZipLatest as BGZip
+from Pipeline.bioinformatics.tools.htslib.tabix.latest import TabixLatest as Tabix
 from Pipeline.bioinformatics.tools.samtools.view.latest import SamToolsViewLatest as SamToolsView
 
 
@@ -73,7 +75,7 @@ class TestGermlinePipeline(unittest.TestCase):
 
     def test_workflow(self):
 
-        w = Workflow("whole-genome-germline-2nd-half")
+        w = Workflow("whole-genome-germline")
 
         reference = Input("reference", FastaWithDict())
         fastqInputs = Input("inputs", Array(Fastq()))
@@ -97,67 +99,64 @@ class TestGermlinePipeline(unittest.TestCase):
         s5_applyBqsr = Step("s5_applyBqsr", Gatk4ApplyBqsr())
         s6_haploy = Step("s6_haploy", Gatk4HaplotypeCaller())
         s7_bcfNorm = Step("s7_bcfNorm", BcfToolsNorm())
-        s8_bgzip = Step("s8_bgzip", BGZipLatest())
-        s9_tabix = Step("s9_tabix", TabixLatest())
+        s8_bgzip = Step("s8_bgzip", BGZip())
+        s9_tabix = Step("s9_tabix", Tabix())
         s10_genotypeConcord = Step("s10_genotypeConcord", Gatk4GenotypeConcordance())
 
-        # # step1
-        # w.add_edge(fastqInputs, s1_sw.fastq)
-        # w.add_edges([
-        #     (s1_inp_reference, s1_sw.reference),
-        #     (s1_inp_header, s1_sw.read_group_header_line),
-        #     (inp_tmpdir, s1_sw.tmpdir)
-        # ])
-        #
-        # #step2
-        # w.add_edge(s1_sw.o3_sortsam, s2_mergeSamFiles.input)
-        # w.add_edge(inp_tmpdir, s2_mergeSamFiles.tmpDir)
-        # w.add_default_value(s2_mergeSamFiles.useThreading, True)
-        # w.add_default_value(s2_mergeSamFiles.createIndex, True)
-        # w.add_default_value(s2_mergeSamFiles.maxRecordsInRam, 5000000)
-        # w.add_default_value(s2_mergeSamFiles.validationStringency, "SILENT")
-        #
-        #
-        # # step3
-        # w.add_edge(s2_mergeSamFiles, s3_markDuplicates)
-        # w.add_edge(inp_tmpdir, s3_markDuplicates.tmpDir)
-        # w.add_default_value(s3_markDuplicates.createIndex, True)
-        # w.add_default_value(s3_markDuplicates.maxRecordsInRam, 5000000)
-        #
-        # #step4 - baserecal
-        # w.add_edges([
-        #     (s3_markDuplicates, s4_baseRecal),
-        #     (reference, s4_baseRecal.reference),
-        #     (s4_inp_SNPS_dbSNP, s4_baseRecal.knownSites),
-        #     (s4_inp_SNPS_1000GP, s4_baseRecal.knownSites),
-        #     (s4_inp_OMNI, s4_baseRecal.knownSites),
-        #     (s4_inp_HAPMAP, s4_baseRecal.knownSites),
-        #     (inp_tmpdir, s4_baseRecal.tmpDir)
-        # ])
-        #
-        # # step5 - apply bqsr
-        # w.add_edges([
-        #     (s3_markDuplicates.output, s5_applyBqsr),
-        #     (s4_baseRecal, s5_applyBqsr),
-        #     (reference, s5_applyBqsr.reference),
-        #     (inp_tmpdir, s5_applyBqsr.tmpDir)
-        # ])
-        #
-        # # step6 - haplotype caller
-        # w.add_edges([
-        #     (s5_applyBqsr, s6_haploy),
-        #     (reference, s6_haploy),
-        #     (s4_inp_SNPS_dbSNP, s6_haploy)
-        # ])
+        # step1
+        w.add_edge(fastqInputs, s1_sw.fastq)
+        w.add_edges([
+            (s1_inp_reference, s1_sw.reference),
+            (s1_inp_header, s1_sw.read_group_header_line),
+            (inp_tmpdir, s1_sw.tmpdir)
+        ])
 
-        # CHEAT TO MAKE THIS QUICKER
-        s7_cheat_inp = Input("s7_inp", VcfIdx())
+        #step2
+        w.add_edge(s1_sw.o3_sortsam, s2_mergeSamFiles.input)
+        w.add_edge(inp_tmpdir, s2_mergeSamFiles.tmpDir)
+        w.add_default_value(s2_mergeSamFiles.useThreading, True)
+        w.add_default_value(s2_mergeSamFiles.createIndex, True)
+        w.add_default_value(s2_mergeSamFiles.maxRecordsInRam, 5000000)
+        w.add_default_value(s2_mergeSamFiles.validationStringency, "SILENT")
+
+
+        # step3
+        w.add_edge(s2_mergeSamFiles, s3_markDuplicates)
+        w.add_edge(inp_tmpdir, s3_markDuplicates.tmpDir)
+        w.add_default_value(s3_markDuplicates.createIndex, True)
+        w.add_default_value(s3_markDuplicates.maxRecordsInRam, 5000000)
+
+        #step4 - baserecal
+        w.add_edges([
+            (s3_markDuplicates, s4_baseRecal),
+            (reference, s4_baseRecal.reference),
+            (s4_inp_SNPS_dbSNP, s4_baseRecal.knownSites),
+            (s4_inp_SNPS_1000GP, s4_baseRecal.knownSites),
+            (s4_inp_OMNI, s4_baseRecal.knownSites),
+            (s4_inp_HAPMAP, s4_baseRecal.knownSites),
+            (inp_tmpdir, s4_baseRecal.tmpDir)
+        ])
+
+        # step5 - apply bqsr
+        w.add_edges([
+            (s3_markDuplicates.output, s5_applyBqsr),
+            (s4_baseRecal, s5_applyBqsr),
+            (reference, s5_applyBqsr.reference),
+            (inp_tmpdir, s5_applyBqsr.tmpDir)
+        ])
+
+        # step6 - haplotype caller
+        w.add_edges([
+            (s5_applyBqsr, s6_haploy),
+            (reference, s6_haploy),
+            (s4_inp_SNPS_dbSNP, s6_haploy)
+        ])
 
         # step7 - BcfToolsNorm
         w.add_edges([
             (reference, s7_bcfNorm),
-            # (s6_haploy, s7_bcfNorm)
-            (s7_cheat_inp, s7_bcfNorm.input)
+            (s6_haploy, s7_bcfNorm.input)
+            # (s7_cheat_inp, s7_bcfNorm.input)
         ])
 
         # step8 - BGZip
@@ -178,27 +177,23 @@ class TestGermlinePipeline(unittest.TestCase):
         # Outputs
 
         w.add_edges([
-            # (s1_sw.o1_bwa, Output("sw_bwa")),
-            # (s1_sw.o2_samtools, Output("sw_samtools")),
-            # (s1_sw.o3_sortsam, Output("sw_sortsam")),
-            # (s2_mergeSamFiles, Output("o4_merged")),
-            # (s3_markDuplicates.output, Output("o5_marked_output")),
-            # (s3_markDuplicates.metrics, Output("o5_marked_metrics")),
-            # (s4_baseRecal, Output("o6_recal")),
-            # (s5_applyBqsr, Output("o7_bqsr")),
-            # (s6_haploy.output, Output("o8_halpo")),
+            (s1_sw.o1_bwa, Output("sw_bwa")),
+            (s1_sw.o2_samtools, Output("sw_samtools")),
+            (s1_sw.o3_sortsam, Output("sw_sortsam")),
+            (s2_mergeSamFiles, Output("o4_merged")),
+            (s3_markDuplicates.output, Output("o5_marked_output")),
+            (s3_markDuplicates.metrics, Output("o5_marked_metrics")),
+            (s4_baseRecal, Output("o6_recal")),
+            (s5_applyBqsr, Output("o7_bqsr")),
+            (s6_haploy.output, Output("o8_halpo")),
             (s7_bcfNorm, Output("o9_bcfnorm")),
             (s8_bgzip, Output("o10_bgzip")),
             (s9_tabix, Output("o11_tabix")),
-            (s10_genotypeConcord.vcf, Output("o12_concord")),
-            (s10_genotypeConcord.summaryMetrics, Output("o12_concord")),
-            (s10_genotypeConcord.detailMetrics, Output("o12_concord")),
-            (s10_genotypeConcord.contingencyMetrics, Output("o12_concord"))
+            (s10_genotypeConcord.summaryMetrics, Output("o12_concord_summary")),
+            (s10_genotypeConcord.detailMetrics, Output("o12_concord_detail")),
+            (s10_genotypeConcord.contingencyMetrics, Output("o12_concord_contig"))
         ])
 
         # w.draw_graph()
         w.dump_cwl(to_disk=True, with_docker=False)
-
-
-        # temporary_directory = Input("tmpdir", Directory(optional=True))
 

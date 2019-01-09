5889b7066c7063d1ecdd5400a43d4f4d60816b85
Michael Franklin <michael@illusional.net>
2019-01-10 09:55:49 +1100

    Refactor WholeGenomeGermline to smaller more discrete subworkflows

--- Pipeline/tests/workflows/wholegenomegermline.py
+++ Pipeline/tests/workflows/wholegenomegermline.py
@@ -1,78 +1,22 @@
 import unittest
 
-from Pipeline import Input, String, Step, Directory, Workflow, Array, Output
-from Pipeline.bioinformatics.data_types.bam import Bam
-from Pipeline.bioinformatics.data_types.bampair import BamPair
-from Pipeline.bioinformatics.data_types.fasta import Fasta
+from Pipeline import Input, String, Step, Directory, Workflow, Array, Output, get_tool
 from Pipeline.bioinformatics.data_types.fastawithdict import FastaWithDict
 from Pipeline.bioinformatics.data_types.fastq import Fastq
-from Pipeline.bioinformatics.data_types.sam import Sam
 from Pipeline.bioinformatics.data_types.vcf import VcfIdx, TabixIdx
 from Pipeline.bioinformatics.tools.bcftools.norm.latest import BcfToolsNormLatest as BcfToolsNorm
-from Pipeline.bioinformatics.tools.bwa.mem.latest import BwaMemLatest as BwaMem
+from Pipeline.bioinformatics.tools.common.alignsortedbam import AlignSortedBam
 from Pipeline.bioinformatics.tools.gatk4.applybqsr.latest import Gatk4ApplyBqsrLatest as Gatk4ApplyBqsr
 from Pipeline.bioinformatics.tools.gatk4.baserecalibrator.latest import \
     Gatk4BaseRecalibratorLatest as Gatk4BaseRecalibrator
-from Pipeline.bioinformatics.tools.gatk4.genotypeconcordance.latest import \
-    Gatk4GenotypeConcordanceLatest as Gatk4GenotypeConcordance
 from Pipeline.bioinformatics.tools.gatk4.haplotypecaller.latest import \
     Gatk4HaplotypeCallerLatest as Gatk4HaplotypeCaller
 from Pipeline.bioinformatics.tools.gatk4.markduplicates.latest import Gatk4MarkDuplicatesLatest as Gatk4MarkDuplicates
 from Pipeline.bioinformatics.tools.gatk4.mergesamfiles.latest import Gatk4MergeSamFilesLatest as Gatk4MergeSamFiles
-from Pipeline.bioinformatics.tools.gatk4.sortsam.latest import Gatk4SortSamLatest as Gatk4SortSam
-from Pipeline.bioinformatics.tools.htslib.bgzip.latest import BGZipLatest as BGZip
-from Pipeline.bioinformatics.tools.htslib.tabix.latest import TabixLatest as Tabix
-from Pipeline.bioinformatics.tools.samtools.view.latest import SamToolsViewLatest as SamToolsView
 from Pipeline.bioinformatics.tools.validation.performancevalidator import PerformanceValidator_1_2_1
 
 
 class TestGermlinePipeline(unittest.TestCase):
-
-
-    def create_subworkflow(self):
-        sw = Workflow("bwa_st_sort")
-
-        s1_bwa = Step("s1_bwa", BwaMem())
-        s2_samtools = Step("s2_samtools", SamToolsView())
-        s3_sortsam = Step("s3_sortsam", Gatk4SortSam())
-
-        s1_inp_header = Input("read_group_header_line", String())
-        s1_inp_reference = Input("reference", Fasta())
-        s1_inp_fastq = Input("fastq", Fastq())
-
-        s3_inp_tmpdir = Input("tmpdir", Directory())
-
-        o1_bwa = Output("o1_bwa", Sam())
-        o2_samtools = Output("o2_samtools", Bam())
-        o3_sortsam = Output("o3_sortsam", BamPair())
-
-        # Fully connect step 1
-        sw.add_edges([
-            (s1_inp_header, s1_bwa.readGroupHeaderLine),
-            (s1_inp_fastq, s1_bwa.reads),
-            (s1_inp_reference, s1_bwa.reference)
-        ])
-        sw.add_default_value(s1_bwa.threads, 36)
-
-        # fully connect step 2
-        sw.add_edge(s1_bwa, s2_samtools.sam)
-
-        # fully connect step 3
-        sw.add_edges([
-            (s2_samtools.out, s3_sortsam.input),
-            (s3_inp_tmpdir, s3_sortsam.tmpDir),
-        ])
-        sw.add_default_value(s3_sortsam.sortOrder, "coordinate")
-        sw.add_default_value(s3_sortsam.createIndex, True)
-        sw.add_default_value(s3_sortsam.validationStringency, "SILENT")
-        sw.add_default_value(s3_sortsam.maxRecordsInRam, 5000000)
-
-        # connect to output
-        sw.add_edge(s1_bwa, o1_bwa)
-        sw.add_edge(s2_samtools, o2_samtools)
-        sw.add_edge(s3_sortsam.output, o3_sortsam)
-
-        return sw
 
     def test_workflow(self):
 
@@ -92,15 +36,13 @@ class TestGermlinePipeline(unittest.TestCase):
 
         inp_tmpdir = Input("tmpdir", Directory())
 
-        s1_sw = Step("sw_bwa_st_sort", self.create_subworkflow())
-
+        s1_sw = Step("sw_bwa_st_sort", AlignSortedBam())
         s2_mergeSamFiles = Step("s2_mergeSamFiles", Gatk4MergeSamFiles())
         s3_markDuplicates = Step("s3_markDuplicates", Gatk4MarkDuplicates())
         s4_baseRecal = Step("s4_baseRecal", Gatk4BaseRecalibrator())
         s5_applyBqsr = Step("s5_applyBqsr", Gatk4ApplyBqsr())
         s6_haploy = Step("s6_haploy", Gatk4HaplotypeCaller())
         s7_bcfNorm = Step("s7_bcfNorm", BcfToolsNorm())
-
         s8_validator = Step("s8_validator", PerformanceValidator_1_2_1())
 
         # step1
@@ -187,7 +129,7 @@ class TestGermlinePipeline(unittest.TestCase):
         ])
 
         # w.draw_graph()
-        w.dump_cwl(to_disk=True, with_docker=False)
+        # w.dump_cwl(to_disk=True, with_docker=False)
 
 
 

1c98ada906750628d20fdbeea16c9a9c452584a0
Michael Franklin <michael@illusional.net>
2019-01-10 09:54:49 +1100

    Adds ability to register workflow as tool: created functionally, no args
    for helpful workflows created as functions (NO PARAMS), ie:
    
    def create_helpful_workflow():
        return Workflow("helpfulWorkflow")
    
    HelpFulWorkflow = create_helpful_workflow
    
    but this is open to changing

--- Pipeline/bioinformatics/__init__.py
+++ Pipeline/bioinformatics/__init__.py
@@ -1,7 +1,10 @@
 import os
 import glob
 import importlib
+from inspect import isclass, isfunction, signature, isabstract
 
+from Pipeline import Workflow
+from Pipeline.tool.tool import Tool
 from constants import PROJECT_ROOT_DIR
 from Pipeline.types.data_types import DataType
 from Pipeline.types.registry import register_type
@@ -9,29 +12,50 @@ from Pipeline.tool.commandtool import CommandTool
 from Pipeline.tool.registry import register_tool
 from Pipeline.utils.logger import Logger
 
+def try_register_type(cls):
+    try:
+        if isabstract(cls): return
+        if isfunction(cls) and len(signature(cls).parameters) == 0:
+            Logger.log(str(cls) + " is an empty-param function")
+            Logger.mute()
+            resolved = cls()
+            Logger.unmute()
+            if isinstance(resolved, Workflow):
+                if register_tool(cls, resolved.id()):
+                    Logger.log("Registered wf as tool: " + resolved.id())
+
+        elif isclass(cls) and issubclass(cls, CommandTool) and cls != CommandTool:
+            Logger.log("attempting to register " + str(cls))
+            if register_tool(cls):
+                Logger.log("Registered tool: " + cls.tool())
+
+        # elif isclass(cls) and issubclass(cls, Workflow) and cls != Workflow:
+        #     identifier = cls().id()
+        #     if register_tool(cls, identifier):
+        #         Logger.log("Registered wf as tool: " + identifier)
+
+        elif isclass(cls) and issubclass(cls, DataType) and cls != DataType:
+            if register_type(cls):
+                Logger.log("Registered type: " + cls.name())
+
+    except Exception as e:
+        import traceback
+        Logger.log_ex(e)
+        Logger.warn(traceback.format_exc())
+
+
 d = os.path.dirname(os.path.abspath(__file__))
-for file in glob.glob(os.path.join(d, "**/*.py"), recursive=True):
-    # name = os.path.splitext(os.path.basename(file))[0]
+files = list(glob.glob(os.path.join(d, "**/*.py"), recursive=True))
+for file in files:
     if os.path.basename(file).startswith("__"):
         continue
 
     name = os.path.splitext(file.replace(PROJECT_ROOT_DIR + "/", ""))[0].replace("/", ".")
     try:
         module = importlib.import_module(name)
-        q = {n: cls for n, cls in list(module.__dict__.items()) if not n.startswith("__") and type(cls) != type }
+        q = {n: cls for n, cls in list(module.__dict__.items()) if not n.startswith("__") and type(cls) != type}
         for cc in q:
-
-            try:
-                cls = q[cc]
-                if issubclass(cls, CommandTool) and cls != CommandTool:
-                    if register_tool(cls):
-                        Logger.log("Registered tool: " + cls.tool())
-
-                elif issubclass(cls, DataType) and cls != DataType:
-                    if register_type(cls):
-                        Logger.log("Registered type: " + cls.name())
-            except:
-                continue
+            try_register_type(q[cc])
 
     except:
         continue

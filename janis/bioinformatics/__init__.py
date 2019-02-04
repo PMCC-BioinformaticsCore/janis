23f1c5e86aa6a3699df7213395039d8ed995460c
Michael Franklin <michael@illusional.net>
2019-02-05 09:45:57 +1100

    Add try / except to janis.bioinformatics to ensure tests don't fail

--- janis/bioinformatics/__init__.py
+++ janis/bioinformatics/__init__.py
@@ -1,1 +1,4 @@
-from janis_bioinformatics import *
+try:
+    from janis_bioinformatics import *
+except ModuleNotFoundError:
+    print("You must install the bioinformatics tools to use this section:\n\t`janis-pipelines[bioinformatics]` OR\n\t`janis-pipelines.bioinformatics`")

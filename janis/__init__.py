b7cc044796ff86c8a010d9f615957b5dad1b685d
Michael Franklin <michael@illusional.net>
2019-04-02 08:39:44 +1100

    Add Tool + Supported translation to janis export

--- janis/__init__.py
+++ janis/__init__.py
@@ -25,7 +25,8 @@ from janis.workflow.step import Step
 from janis.workflow.input import Input
 from janis.workflow.output import Output
 from janis.tool.commandtool import CommandTool
-from janis.tool.tool import ToolArgument, ToolInput, ToolOutput
+from janis.tool.tool import Tool, ToolArgument, ToolInput, ToolOutput
+from janis.translations import SupportedTranslations
 from janis.types import InputSelector, WildcardSelector
 from janis.types.data_types import DataType
 from janis.types.common_data_types import Boolean, String, Int, Float, Double, File, Directory, Array, Filename, Stdout

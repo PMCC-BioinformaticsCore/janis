13845bfb239ddff3d31f5149586d47f209b3320a
Michael Franklin <michael@illusional.net>
2019-04-11 15:54:51 +1000

    Add CPU and Memory Selectors

--- janis/types/__init__.py
+++ janis/types/__init__.py
@@ -17,3 +17,13 @@ class InputSelector(Selector):
 class WildcardSelector(Selector):
     def __init__(self, wildcard):
         self.wildcard = wildcard
+
+
+class MemorySelector(Selector):
+    def __init__(self, suffix=None, prefix=None):
+        self.suffix = suffix
+        self.prefix = prefix
+
+
+class CpuSelector(Selector):
+    pass

b459c7261a6801aa44ed3834606803c79b774dda
Michael Franklin <michael@illusional.net>
2019-05-02 15:59:25 +1000

    Rewrite translation boundary to use the new CWL + WDL translator classes

--- janis/translations/__init__.py
+++ janis/translations/__init__.py
@@ -1,7 +1,11 @@
+from typing import Dict, Optional, Any
+
+from janis.translations.wdl import WdlTranslator
 from janis.utils.janisconstants import GITHUB_URL
 from janis.translations.exportpath import ExportPathKeywords
-from . import cwl, wdl
-from .wdl import dump_wdl as wdl_dump_workflow
+from .translationbase import TranslatorBase
+from .cwl import CwlTranslator
+
 
 SupportedTranslation = str
 
@@ -11,46 +15,37 @@ class SupportedTranslations:
     WDL: SupportedTranslation = "wdl"
 
 
+def get_translator(translation: SupportedTranslation) -> TranslatorBase:
+    if translation == SupportedTranslations.CWL:
+        return CwlTranslator()
+    elif translation == SupportedTranslations.WDL:
+        return WdlTranslator()
+
+    raise NotImplementedError(f"The requested translation ('{translation}') has not been implemented yet, "
+                              f"why not contribute one at '{GITHUB_URL}'.")
+
+
 def translate_workflow(workflow, translation: SupportedTranslation,
                        to_console=True, with_docker=True, with_resource_overrides=False, to_disk=False,
                        export_path=ExportPathKeywords.default, write_inputs_file=False, should_validate=False,
                        should_zip=True):
-    if translation == SupportedTranslations.CWL:
-        return cwl.dump_cwl(workflow, to_console=to_console, with_docker=with_docker,
-                            with_resource_overrides=with_resource_overrides, to_disk=to_disk,
-                            export_path=export_path, write_inputs_file=write_inputs_file,
-                            should_validate=should_validate, should_zip=should_zip)
-
-    elif translation == SupportedTranslations.WDL:
-        return wdl.dump_wdl(workflow, to_console=to_console, with_docker=with_docker,
-                            with_resource_overrides=with_resource_overrides, to_disk=to_disk,
-                            export_path=export_path, write_inputs_file=write_inputs_file,
-                            should_validate=should_validate, should_zip=should_zip)
-
-    else:
-        raise NotImplementedError(f"The requested translation ('{translation}') has not been implemented yet, "
-                                  f"why not contribute one at '{GITHUB_URL}'.")
+    translator = get_translator(translation)
+    return translator.translate(
+        workflow, to_console=to_console, with_docker=with_docker,
+        with_resource_overrides=with_resource_overrides, to_disk=to_disk,
+        export_path=export_path, write_inputs_file=write_inputs_file,
+        should_validate=should_validate, should_zip=should_zip
+    )
 
 
 def translate_tool(tool, translation: SupportedTranslation, with_docker, with_resource_overrides=False):
-    if translation == SupportedTranslations.CWL:
-        return cwl.translate_tool_str(tool, with_docker=with_docker, with_resource_overrides=with_resource_overrides)
-
-    elif translation == SupportedTranslations.WDL:
-        return wdl.translate_tool_str(tool, with_docker=with_docker)
-
-    else:
-        raise NotImplementedError(f"The requested translation ('{translation}') has not been implemented yet, "
-                                  f"why not contribute one at '{GITHUB_URL}'.")
+    translator = get_translator(translation)
+    return translator.stringify_translated_tool(translator.translate_tool(
+        tool, with_docker=with_docker,
+        with_resource_overrides=with_resource_overrides)
+    )
 
 
-def build_resources_input(workflow, translation: SupportedTranslation, hints):
-    if translation == SupportedTranslations.CWL:
-        return cwl.build_cwl_resource_inputs_dict(workflow, hints)
-
-    elif translation == SupportedTranslations.WDL:
-        return wdl.build_wdl_resource_inputs_dict(workflow, hints)
-
-    else:
-        raise NotImplementedError(f"The requested translation ('{translation}') has not been implemented yet, "
-                                  f"why not contribute one at '{GITHUB_URL}'.")
+def build_resources_input(workflow, translation: SupportedTranslation, hints) -> str:
+    translator = get_translator(translation)
+    return translator.stringify_translated_inputs(translator.build_resources_input(workflow, hints))

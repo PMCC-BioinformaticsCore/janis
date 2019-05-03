13bedb5441b982957f88d410126a97a9e9c751ab
Michael Franklin <michael@illusional.net>
2019-05-03 15:24:19 +1000

    Rewrite simple workflow for tutorial / workshop

--- janis/examples/simple.py
+++ janis/examples/simple.py
@@ -1,24 +1,40 @@
-import unittest
+"""
+simple.py
 
-from janis.hints import CaptureType
+    Date: 2019-05-03
+    Author: Michael Franklin
 
-from janis.unix.tools.debugecho import DebugEchoInputs
+
+    This class represents a simple unix workflow, and was prepared as part of the
+    following tutorial: https://janis.readthedocs.io/en/latest/tutorials/simple.html
+
+    Workflow description:
+        1. A file is untarred
+        2. The result of the untar is compiled
+        3. The (untarred + compiled) result is tarred into a new archive
+
+        The following ASCII diagram is a basic visual representation of the DAG:
+
+        file.tar → untar → compile → tar -> out.tar
+                          ↘_______↗
+"""
+
+# The classes we require to build a basic workflow
+from janis import Workflow, Input, Output, Step, File
+
+# Data types - These help us logically connect workflows
+from janis.unix.data_types.tarfile import TarFile
+
+# Tools - The command line tools we're going to call
+from janis.unix.tools.compile import Compile
 from janis.unix.tools.tar import Tar
 from janis.unix.tools.untar import Untar
-from janis.unix.tools.compile import Compile
-from janis.unix.data_types.tar_file import TarFile
-
-from janis import Workflow, Input, Output, Step, String, File, Logger
-
-# file.tar -> untar -> compile -> tar -> out.tar
-#                  \_____________↗
 
 
-class TestSimple(unittest.TestCase):
+class SimpleWorkflow(Workflow):
 
-    def test_workflow(self):
-        # Logger.mute()
-        w = Workflow("simple")
+    def __init__(self):
+        super().__init__("simpleWorkflow")
 
         inp1 = Input("tarFile", TarFile())
 
@@ -28,45 +44,15 @@ class TestSimple(unittest.TestCase):
 
         outp = Output("out", File())
 
-        w.add_edge(inp1, step1.tarFile)
-        w.add_edge(step1.files, step2.file)
-
-        w.add_edge(inp1, step1.tarFile)
-        w.add_edge(step1.files, step2.file)     # Auto scatter
-        w.add_edge(step1.files, step3.files)
-        w.add_edge(step2.compiled, step3.files)
-        w.add_edge(step3.tarred, outp)
-        # Logger.unmute()
-
-        w.dump_translation("wdl", to_disk=False, with_resource_overrides=True)
-        print(w.generate_resources_file("wdl", {CaptureType.KEY: CaptureType.CHROMOSOME}))
+        self.add_edge(inp1, step1.tarFile)
+        self.add_edge(step1.files, step2.file)  # Auto scatter
+        self.add_edge(step1.files, step3.files)
+        self.add_edge(step2.compiled, step3.files)
+        self.add_edge(step3.tarred, outp)
 
 
-        return w
-
-    def test_pipe(self):
-
-        # Logger.mute()
-        w = Workflow("simple")
-
-        inp1 = Input("tarFile", TarFile(), "/Users/franklinmichael/Desktop/workflows-for-testing/03-simple/inputs/hello.tar")
-        step1 = Step("untar", Untar())
-        step2 = Step("compile", Compile())
-        step3 = Step("tar", Tar())
-        # debug = Step("debug", DebugEchoInputs())
-
-        w.add_pipe(inp1, step1, step2, step3.files, Output("out"))
-        w.add_edge(step1, step3.files2)
-        # w.add_edge(step3.tarred, debug.fileInput)
-        # w.add_edge(debug, Output("debugout"))
-
-        # w.draw_graph()
-        w.dump_translation(translation="cwl", to_disk=True, with_resource_overrides=False)
-        # w.dump_wdl(to_disk=True, with_docker=True)
-
-        # Logger.unmute()
-
-        return w
+if __name__ == "__main__":
+    SimpleWorkflow().translate("wdl")
 
 
 

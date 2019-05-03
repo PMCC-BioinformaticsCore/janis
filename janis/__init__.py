51484461ef0614eb0f9d428e9125aed0b48107ae
Michael Franklin <michael@illusional.net>
2019-05-03 15:22:36 +1000

    Cleanup janis imports

--- janis/__init__.py
+++ janis/__init__.py
@@ -20,16 +20,18 @@ Some terminology:
 
 __version__ = "v0.2.14"
 
-from janis.workflow.workflow import Workflow
-from janis.workflow.step import Step
-from janis.workflow.input import Input
-from janis.workflow.output import Output
+
+from janis.hints import CaptureType
 from janis.tool.commandtool import CommandTool
 from janis.tool.tool import Tool, ToolArgument, ToolInput, ToolOutput
 from janis.translations import SupportedTranslations
 from janis.types import InputSelector, WildcardSelector
-from janis.types.data_types import DataType
 from janis.types.common_data_types import Boolean, String, Int, Float, Double, File, Directory, Array, Filename, Stdout
+from janis.types.data_types import DataType
+from janis.unix import *
 from janis.utils.logger import Logger, LogLevel
 from janis.utils.metadata import Metadata, WorkflowMetadata, ToolMetadata
-from janis.unix import *
+from janis.workflow.input import Input
+from janis.workflow.output import Output
+from janis.workflow.step import Step
+from janis.workflow.workflow import Workflow

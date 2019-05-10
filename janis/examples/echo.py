a6ffe67ef4d845f6f79a08965c4ddde1370961b8
Michael Franklin <michael@illusional.net>
2019-05-10 15:01:00 +1000

    Remove refs to running wf from Echo example

--- janis/examples/echo.py
+++ janis/examples/echo.py
@@ -11,10 +11,3 @@ w.add_pipe(inp, step, outp)
 
 # Will print the CWL, input file and relevant tools to the console
 # w.translate("wdl", to_disk=True, write_inputs_file=True)
-
-if __name__ == "__main__":
-    import shepherd
-
-    eng = shepherd.Cromwell(environment="pmac")
-    task = shepherd.from_janis(w, engine=eng)
-    print(task.outputs)

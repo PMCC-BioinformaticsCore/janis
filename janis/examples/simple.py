8c48c8394db3fddc5933801d5e9a64fe902354cf
Michael Franklin <michael@illusional.net>
2019-07-05 13:07:29 +1000

    Update simple example to use fully qualified connections

--- janis/examples/simple.py
+++ janis/examples/simple.py
@@ -20,7 +20,7 @@ simple.py
 """
 
 # The classes we require to build a basic workflow
-from janis import Workflow, Input, Output, Step, File
+from janis import Workflow, Input, Output, Step
 
 # Data types - These help us logically connect workflows
 from janis.unix.data_types.tarfile import TarFile
@@ -32,27 +32,23 @@ from janis.unix.tools.untar import Untar
 
 
 class SimpleWorkflow(Workflow):
-
     def __init__(self):
         super().__init__("simpleWorkflow")
 
-        inp = Input("tarFile", TarFile())
+        inp = Input("tarFile", TarFile(), value="/path/to/hello.tar")
 
-        step1 = Step("untar", Untar())
-        step2 = Step("compile", Compile())
-        step3 = Step("tar", Tar())
+        untar = Step("untar", Untar())
+        compile = Step("compile", Compile())
+        retar = Step("tar", Tar())
 
         outp = Output("out")
 
-        self.add_edge(inp, step1.tarFile)
-        self.add_edge(step1.files, step2.file)  # Auto scatter
-        self.add_edge(step1.files, step3.files)
-        self.add_edge(step2.compiled, step3.files2)
-        self.add_edge(step3.tarred, outp)
+        self.add_edge(inp, untar.tarFiles)
+        self.add_edge(untar.files, compile.file)  # Auto scatter
+        self.add_edge(untar.files, retar.files)
+        self.add_edge(compile.compiled, retar.files2)
+        self.add_edge(retar.tarred, outp)
 
 
 if __name__ == "__main__":
     SimpleWorkflow().translate("wdl", to_disk=True, merge_resources=True)
-
-
-

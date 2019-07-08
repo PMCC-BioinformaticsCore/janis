070f7fd44a34afb4e229e83ec72cad155fc1ecdc
Michael Franklin <michael@illusional.net>
2019-07-08 15:25:17 +1000

    Fully qualify userguide example connections

--- janis/examples/userguide_workflow.py
+++ janis/examples/userguide_workflow.py
@@ -5,14 +5,18 @@ from janis.unix.tools.untar import Untar
 
 w = Workflow("user_guide")
 
-tarball = Input("tarball", TarFile())
-compiled_class = Output("compiled_class")
+tarball = Input("tarball", TarFile(), value="path/to/tar")
+compiled_class = Output("compiledOutput")
 
 untar = Step("untar", Untar())
 compile_step = Step("compile", Compile())
 
-w.add_edge(tarball, untar.tarFile)
-w.add_edge(untar, compile_step)
-w.add_edge(compile_step, compiled_class)
+w.add_edges(
+    [
+        (tarball, untar.tarFile),
+        (untar.files, compile_step.file),
+        (compile_step.compiled, compiled_class),
+    ]
+)
 
-w.translate("cwl", to_disk=False)
+w.translate("cwl", to_disk=False)  # or "wdl"

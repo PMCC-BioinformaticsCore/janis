05d1815279ae3b81b12163df0ff7afb46c1bd7f3
Michael Franklin <michael.franklin@petermac.org>
2019-07-22 14:34:14 +1000

    Update docs

--- janis/__init__.py
+++ janis/__init__.py
@@ -1,22 +1,23 @@
 """
-Welcome to the source code for Janis!
+Welcome to the source code for Janis! This module is mostly a placeholder and
+used to bundle the different components of Janis (without crazy circular deps).
+It also allows you to add your own tool sheds by including the:
+
+    'janis.extension` entry-point.
+
+Here's an amazing guide on entry-points:
+    https://amir.rachum.com/blog/2017/07/28/python-entry-points/
+
+Here are some of the tool sheds that are already built:
+
+    - unix: janis-pipelines.unix (
+    - bioinformatics: janis-pipelines.bioinformatics (https://github.com/PMCC-BioinformaticsCore/janis-bioinformatics)
+
+There's also a workflow runner:
+    - runner: janis-pipelines.runner (https://github.com/PMCC-BioinformaticsCore/janis-runner)
 
 Janis is a framework creating specialised, simple workflow definitions that are
 transpiled to Common Workflow Language or Workflow Definition Language.
-
-Below you'll find the classes you can access by importing Janis:
-
->>  import janis as j
-
-Some noteworthy classes are Workflow, CommandTool and the janis.translations module
-
-Some terminology:
-    - Edge:
-        - An edge may exist between 2 nodes, it represents a dependency
-        - Every edge will have a source_map which is indexed by the tag of the input on the finish node
-        - This source_map value is singular or a list, as you can connect multiple sources to one input
-
-
 
 _Proudly made on Planet Earth._
 
@@ -26,8 +27,6 @@ import sys, os
 import pkg_resources
 
 from janis_core import *
-
-runner = None
 
 for entrypoint in pkg_resources.iter_entry_points(group="janis.extension"):
     try:

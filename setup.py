0addf929d269851fcdffbfdee688f072cca3bdf4
Michael Franklin <michael.franklin@petermac.org>
2020-03-20 19:44:51 +1100

    Bump janis project deps

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 # Version information is found in the __init__ file of `janis/`
 DESCRIPTION = "Contains classes and helpers to build a workflow, and provide options to convert to CWL / WDL"
 
-JANIS_CORE_VERSION = "v0.9.6"
-JANIS_ASSISTANT_VERSION = "v0.9.8"
+JANIS_CORE_VERSION = "v0.9.9"
+JANIS_ASSISTANT_VERSION = "v0.9.11"
 JANIS_UNIX_VERSION = "v0.9.0"
-JANIS_BIOINFORMATICS_VERSION = "v0.9.4"
-JANIS_PIPELINES_VERSION = "v0.9.0"
-JANIS_TEMPLATES_VERSION = "v0.9.0"
+JANIS_BIOINFORMATICS_VERSION = "v0.9.6"
+JANIS_PIPELINES_VERSION = "v0.9.3"
+JANIS_TEMPLATES_VERSION = "v0.9.4"
 
 
 ######## SHOULDN'T NEED EDITS BELOW THIS LINE ########
